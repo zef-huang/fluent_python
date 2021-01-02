af931a509cb1a5daa61f51c9e01c5e86d664e368
huangzefeng <737244264@qq.com>
2021-01-02 22:58:41 +0800

    python 装饰器

---
+++ decorator/decorator.py
@@ -0,0 +1,23 @@
+# coding=utf-8
+# 手写一个装饰器
+
+
+def training_decorator(func):
+    def wrapper(*args, **kwargs):
+        print("training 3 hours ...")
+        print("training done")
+
+        func(*args, **kwargs)
+    
+    return wrapper
+
+
+def eat_something(something):
+    print("you can eat %s now" % something)
+
+
+
+if __name__ == '__main__':
+    eat_something = training_decorator(eat_something)
+    eat_something('two eggs')
+    
