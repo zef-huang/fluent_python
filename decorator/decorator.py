393dbd08411e39831b5dbabe02cf1be1d0663016
huangzefeng <737244264@qq.com>
2021-01-03 13:49:12 +0800

    装饰器没有办法传入参数，所以通过一个函数，接收传入的参数，并且返回装饰器即可

--- decorator/decorator.py
+++ decorator/decorator.py
@@ -13,15 +13,18 @@ def training_decorator(func):
 
 
 # 案例二：实现一个简单的装饰器，用于打印函数的执行时间
-from  datetime import datetime
-def rerord_func_run_time(func):
-    def wrapper(*args, **kwargs):
-        print(datetime.now())
-        func(*args, **kwargs)
-    return wrapper
+from  time import time
+def run_time_decorator(func_name):
+    def rerord_func_run_time(func):
+        def wrapper(*args, **kwargs):
+            start_time = time()
+            func(*args, **kwargs)
+            print('函数 %s 执行时间：%s' % (func_name, time() - start_time))
+        return wrapper
+    return rerord_func_run_time
 
 
-@rerord_func_run_time
+@run_time_decorator("eat_something")
 @training_decorator
 def eat_something(something):
     print("you can eat %s now" % something)
