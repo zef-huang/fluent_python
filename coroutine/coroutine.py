8a5ae4a3f5306bb40ff08f04a3541134bb635fd1
huangzefeng <737244264@qq.com>
2021-01-30 22:24:18 +0800

    实现求平均值的协程

--- coroutine/coroutine.py
+++ coroutine/coroutine.py
@@ -25,6 +25,40 @@ def main():
     # third = next(cor)
     print("see the first data", third)
 
+
+# =============================================================
+# 2. 实现一个求平均值的协程
+# =============================================================
+
+
+def avg_coro():
+    count = 0
+    sum = 0
+    while True:
+        recieve_number = yield
+        if recieve_number:
+            sum += recieve_number
+            count += 1
+        else:
+            break
+    
+    return sum/count  if count else 0
+
+
+def main2():
+    generator = avg_coro()
+    # 预激活
+    next(generator)
+
+    generator.send(3)
+    generator.send(33)
+    generator.send(33)
+    try:
+        generator.send('')
+    except Exception as result:
+        print("avg is ", result)
+
+
 if __name__ == '__main__':
-    main()
+    main2()
     
